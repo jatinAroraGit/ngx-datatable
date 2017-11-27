9f9e90a397c9cf97a7e16165b798228dc6d6202b
Sanborn <sanborn.sen@gmail.com>
2017-11-27 12:10:28 +0530

    A fullscreen tree demo component added to verify the implementation

--- demo/app.component.ts
+++ demo/app.component.ts
@@ -56,6 +56,12 @@ import { Location, LocationStrategy, HashLocationStrategy } from '@angular/commo
             </ul>
           </li>
           <li>
+            <h4>Tree</h4>
+            <ul>
+              <li><a href="#fullscreen-tree" (click)="state='fullscreen-tree'">Full screen Tree</a></li>
+            </ul>
+          </li>
+          <li>
             <h4>Rows</h4>
             <ul>
               <li><a href="#row-grouping" (click)="state='row-grouping'">Row Grouping</a></li>
@@ -148,6 +154,9 @@ import { Location, LocationStrategy, HashLocationStrategy } from '@angular/commo
         <!-- Themes -->
         <basic-dark-theme-demo *ngIf="state === 'dark'"></basic-dark-theme-demo>
         <basic-bootstrap-theme-demo  *ngIf="state === 'bootstrap'"></basic-bootstrap-theme-demo>
+
+        <!-- Tree -->
+        <full-screen-tree-demo *ngIf="state === 'fullscreen-tree'"></full-screen-tree-demo>
 
         <!-- Paging -->
         <row-grouping-demo *ngIf="state === 'row-grouping'"></row-grouping-demo>
--- demo/module.ts
+++ demo/module.ts
@@ -44,6 +44,9 @@ import { ClientSortingComponent } from './sorting/sorting-client.component';
 import { InlineTemplatesComponent } from './templates/template-dom.component';
 import { TemplateRefTemplatesComponent } from './templates/template-obj.component';
 
+// -- Tree
+import { FullScreenTreeComponent } from './tree/fullscreen.component';
+
 // -- Selection
 import { CellSelectionComponent } from './selection/selection-cell.component';
 import { MultiSelectionComponent } from './selection/selection-multi.component';
@@ -73,6 +76,7 @@ import { SummaryRowInlineHtmlComponent } from './summary/summary-row-inline-html
     BasicAutoComponent,
     BasicFixedComponent,
     FullScreenComponent,
+    FullScreenTreeComponent,
     InlineEditComponent,
     VirtualScrollComponent,
     HorzVertScrolling,
